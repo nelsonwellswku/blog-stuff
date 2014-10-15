2a0615c42924bc2e89c25d0b83de223fd52ea655
Nelson Wells <nelsonwellswku@gmail.com>
2014-10-14 20:52:54 -0500

    Should should be blog entry

---
+++ should-shouldbe-a-silly-mistake/FluentAssertionsShouldShouldBe/MistakesTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+
+namespace FluentAssertionsShouldShouldBe
+{
+	internal class MistakesTests
+	{
+		[Test]
+		public void ThisDumbThingIKeepDoing()
+		{
+			var thing = new Thing { FirstName = "Nelson" };
+			var thing2 = new Thing { FirstName = "Nelson" };
+
+			// This should definitely pass, right?
+			thing.Should().ShouldBeEquivalentTo(thing2);
+		}
+
+		private class Thing
+		{
+			public string FirstName { get; set; }
+		}
+	}
+}
