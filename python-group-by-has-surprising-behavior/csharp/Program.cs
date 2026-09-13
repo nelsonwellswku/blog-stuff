0cce027d15b67ccff7589852eb64648205e4952b
Nelson Wells <nelsonwellswku@gmail.com>
2026-09-13 16:59:26 -0500

    add python-group-by article code

---
+++ python-group-by-has-surprising-behavior/csharp/Program.cs
@@ -0,0 +1,30 @@
+namespace Octogami.GroupByDemo;
+
+public class Person
+{
+    public int CoupleId { get; set; }
+    public string? Name { get; set; }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        List<Person> friends = [
+            new Person {CoupleId = 3, Name = "John"},
+            new Person {CoupleId = 1, Name = "Thomas"},
+            new Person {CoupleId = 2, Name = "Eugene"},
+            new Person {CoupleId = 2, Name = "Beatrice"},
+            new Person {CoupleId = 1, Name = "Imani"},
+            new Person {CoupleId = 3, Name = "Mary"}
+        ];
+
+        var grouped_friends = friends.GroupBy(x => x.CoupleId);
+        foreach (var group in grouped_friends)
+        {
+            var coupleId = group.Key;
+            var names = string.Join(" ", group.Select(x => x.Name));
+            Console.WriteLine($"{coupleId} {names}");
+        }
+    }
+}
---
+++ python-group-by-has-surprising-behavior/python/main.py
@@ -0,0 +1,20 @@
+from itertools import groupby
+
+def main():
+    friends = [
+        {"couple_id": 3, "name": "John"},
+        {"couple_id": 1, "name": "Thomas"},
+        {"couple_id": 2, "name": "Eugene"},
+        {"couple_id": 2, "name": "Beatrice"},
+        {"couple_id": 1, "name": "Imani"},
+        {"couple_id": 3, "name": "Mary"}
+    ]
+
+    friends_groups = groupby(friends, lambda x: x["couple_id"])
+
+    for (key, grouping) in friends_groups:
+        print(key, "-->", " ".join([obj["name"] for obj in grouping]))
+
+
+if __name__ == "__main__":
+    main()
